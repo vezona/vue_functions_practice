920f6fbacc1036fc4524adb5c6be970587eafe7f
vezonaChin <vezona5tv@gmail.com>
2023-07-19 17:57:22 +0800

    getBoundingClientRect

--- src/components/time-select.vue
+++ src/components/time-select.vue
@@ -1,8 +1,12 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, watch, onMounted, nextTick } from "vue";
 import multiToggle from "/src/compose/useMultiToggle";
 
 const props = defineProps({
+  isShowSelectOptions: {
+    type: Boolean,
+    defalut: false,
+  },
   time: {
     type: String,
     required: true,
@@ -39,7 +43,12 @@ for (let i = 0; i < hours.length; i++) {
   }
 }
 
-const emit = defineEmits(["update:time", "update:hours", "update:minutes"]);
+const emit = defineEmits([
+  "update:time",
+  "update:hours",
+  "update:minutes",
+  "update:isShowSelectOptions",
+]);
 
 const choosedTime = computed({
   get: () => props.time.replace(/\s*/g, ""),
@@ -64,9 +73,40 @@ const handleChooseTime = (item) => {
   choosedHours.value = item.value.hours;
   choosedMinutes.value = item.value.minutes;
 };
+
+const elRef = ref(null);
+const initPosition = ref(0);
+const currentPosition = ref(0);
+const marginTop = ref(0);
+const padding = 8; //父層元素的padding
+onMounted(async () => {
+  nextTick(() => {
+    if (elRef.value) {
+      initPosition.value = elRef.value.getBoundingClientRect().bottom;
+      console.log("init", initPosition.value);
+    }
+  });
+});
+
+//是否展開下拉選單
+watch(currentSelect, () => {
+  emit("update:isShowSelectOptions", currentSelect.value === "timePicker");
+
+  if (currentSelect.value === "timePicker" && elRef.value) {
+    currentPosition.value = elRef.value.getBoundingClientRect().bottom;
+
+    console.log("curr", currentPosition.value);
+    console.log("diff", currentPosition.value - initPosition.value);
+    if (currentPosition.value - initPosition.value > 0) {
+      marginTop.value = 0;
+    } else {
+      marginTop.value = currentPosition.value - initPosition.value;
+    }
+  }
+});
 </script>
 <template>
-  <div class="w-30">
+  <div ref="elRef" class="w-30">
     <div
       data-item-id="caseType"
       class="cursor-pointer text-center relative"
@@ -80,9 +120,13 @@ const handleChooseTime = (item) => {
       <!-- items -->
       <div
         v-show="currentSelect === 'timePicker'"
-        class="border bg-gray-100 shadow-lg w-full py-2 top-10 z-20 absolute"
+        class="border bg-gray-100 shadow-lg z-20"
+        :class="[isShowSelectOptions ? 'fixed w-30' : 'absolute w-full top-10']"
+        :style="{ marginTop: marginTop + 'px' }"
       >
-        <ul class="pl-3 overflow-y-scroll scroll-wrap !max-h-60">
+        <ul
+          class="max-h-50 pl-3 overflow-y-scroll scroll-wrap-test select-none"
+        >
           <li
             v-for="item in timeList"
             :key="item.key"
@@ -99,3 +143,29 @@ const handleChooseTime = (item) => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.scroll-wrap-test {
+  --scrollbar-track: initial;
+  --scrollbar-thumb: initial;
+  scrollbar-color: var(--scrollbar-thumb) var(--scrollbar-track);
+  overflow: overlay;
+  scrollbar-width: thin;
+  --scrollbar-thumb: #b5a99d;
+  --scrollbar-track: #e9ecef;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.scroll-wrap-left-test {
+  --scrollbar-track: initial;
+  --scrollbar-thumb: initial;
+  scrollbar-color: var(--scrollbar-thumb) var(--scrollbar-track);
+  overflow: overlay;
+  scrollbar-width: thin;
+  --scrollbar-thumb: #b5a99d;
+  --scrollbar-track: #e9ecef;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+</style>
--- src/views/dropdownSelect.vue
+++ src/views/dropdownSelect.vue
@@ -1,46 +1,42 @@
 <template>
-  <div
-    class="border flex border-gray-700 w-full justify-center overflow-y-auto scroll-wrap"
-    :class="[false ? 'overflow-y-auto scroll-wrap' : 'relative']"
-  >
-    <section class="rounded flex h-50vh w-full py-5 justify-center">
-      <ul class="basis-9/16">
-        <li
-          v-for="(
-            i, index
-          ) in caseAutoRefreshList.autoRefreshSettingInfoSystemGift"
-          :key="i.recordId"
-          class="flex py-2 items-center first:pt-0"
-        >
-          <span class="text-right pr-2 text-c-dark-500 w-7">{{
-            `${index + 1}.`
-          }}</span>
-          <time-select
-            v-model:time="i.dailyRefreshTime"
-            v-model:hours="i.regularUpdateTimeHour"
-            v-model:minutes="i.regularUpdateTimeMin"
-          />
-          <div class="pl-2 text-c-dark-500">
-            <span class="text-sm pl-1"> 今天到期 </span>
-          </div>
-        </li>
-
-        <select name="pets" id="pet-select">
-          <option value="">--Please choose an option--</option>
-          <option value="dog">Dog</option>
-          <option value="cat">Cat</option>
-          <option value="hamster">Hamster</option>
-          <option value="parrot">Parrot</option>
-          <option value="spider">Spider</option>
-          <option value="goldfish">Goldfish</option>
-        </select>
-      </ul>
-
-      u
-    </section>
+  <div class="border border-gray-400 px-10 pb-10 block">
+    <p class="">isShowSelectOptions: {{ isShowSelectOptions }}</p>
+    <div
+      class="border flex mx-auto border-gray-700 w-50vw justify-center"
+      :class="[
+        isShowSelectOptions
+          ? 'overflow-y-hidden'
+          : 'overflow-y-auto scroll-wrap-test',
+      ]"
+    >
+      <section class="rounded flex h-50vh w-full py-5 justify-center">
+        <ul class="basis-9/16">
+          <li
+            v-for="(
+              i, index
+            ) in caseAutoRefreshList.autoRefreshSettingInfoSystemGift"
+            :key="i.recordId"
+            class="flex py-2 items-center first:pt-0"
+          >
+            <span class="text-right pr-2 text-c-dark-500 w-7">{{
+              `${index + 1}.`
+            }}</span>
+            <time-select
+              v-model:isShowSelectOptions="isShowSelectOptions"
+              v-model:time="i.dailyRefreshTime"
+              v-model:hours="i.regularUpdateTimeHour"
+              v-model:minutes="i.regularUpdateTimeMin"
+            />
+            <div class="pl-2 text-c-dark-500">
+              <span class="text-sm pl-1"> 今天到期 </span>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
   </div>
 
-  <!-- ==== -->
+  <!-- OuterWrap / InnerWrap -->
   <div class="cursor-pointer mt-10" @click="iswrap = !iswrap">
     Click {{ iswrap }}
   </div>
@@ -68,6 +64,7 @@
 <script setup>
 import { ref, computed } from "vue";
 const iswrap = ref(true);
+const isShowSelectOptions = ref(false);
 const caseAutoRefreshList = ref({
   autoRefreshSettingInfoSystemGift: [
     {
@@ -154,3 +151,29 @@ const caseAutoRefreshList = ref({
   ],
 });
 </script>
+
+<style scoped>
+.scroll-wrap-test {
+  --scrollbar-track: initial;
+  --scrollbar-thumb: initial;
+  scrollbar-color: var(--scrollbar-thumb) var(--scrollbar-track);
+  overflow: overlay;
+  scrollbar-width: thin;
+  --scrollbar-thumb: #b5a99d;
+  --scrollbar-track: #e9ecef;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.scroll-wrap-left-test {
+  --scrollbar-track: initial;
+  --scrollbar-thumb: initial;
+  scrollbar-color: var(--scrollbar-thumb) var(--scrollbar-track);
+  overflow: overlay;
+  scrollbar-width: thin;
+  --scrollbar-thumb: #b5a99d;
+  --scrollbar-track: #e9ecef;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+</style>
